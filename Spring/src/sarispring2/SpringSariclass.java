4f7c8b553fc0b75d053f60e862c037da4536cb26
Saritha-Velpuri <62092195+Saritha-Velpuri@users.noreply.github.com>
2020-05-21 05:11:02 -0700

    Add files via upload

---
+++ Spring/src/sarispring2/SpringSariclass.java
@@ -0,0 +1,22 @@
+package sarispring2;
+
+	import org.springframework.beans.factory.BeanFactory;
+	import org.springframework.beans.factory.xml.XmlBeanFactory;
+	import org.springframework.context.ApplicationContext;
+	import org.springframework.context.support.ClassPathXmlApplicationContext;
+	import org.springframework.core.io.FileSystemResource;
+
+	public class Springsariclass {
+
+		public static void main(String[] args) {
+			//Rectangle rectangle=new Rectangle();
+			
+			//BeanFactory factory=new XmlBeanFactory(new FileSystemResource("spring.xml"));
+			ApplicationContext context=new ClassPathXmlApplicationContext("spring.xml");
+			Triangle triangle=(Triangle)context.getBean("triangle");
+			triangle.draw();
+		}
+
+	}
+
+}
